f98faa1379da3586917c4e05a1eef8b146592df2
Kayla Webb <kaylarwebb@gmail.com>
2017-11-06 17:03:05 -0500

    Added divs to html, more pseudocode

--- QuizApp/index.js
+++ QuizApp/index.js
@@ -1,12 +1,10 @@
 /* global $ */
 'use strict';
 
-//define data stores 
-//question, answer1, answer2, answer3, answer4, answer5, correct, userChoice 
-
 /******************************************************** 
- * Step 1: Define objects & database ********************
- * ************************************/
+Step 1: Define objects & database 
+********************************************************/
+
 const QUESTIONS = [
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
@@ -25,12 +23,13 @@ const STORE = {
   currentRadioButtonChoice: 0,
   currentScore: 0,
   lastQuestionAnswered: 0,
+  lastQuestionCorrect: false,
+  quizSummaryHTML:''
 };
 
 /******************************************************** 
  * Step 2: Define functions that process user interaction 
  ********************************************************/
-
 
 function respondToUserButton() {
     //Set the currentView and what is next?
@@ -40,18 +39,20 @@ function respondToUserButton() {
       //call renderQuizPage();
     //else if currentView === 1
      //currentView = 2
-    //if currentView === 2
-     //call
+     //renderFeedback()
+    //else if currentView === 2
+     //if question===10 then currentView = 3
+      //else currentView = 1
+    //else if currentView === 3
+      //currentView = 0
+      //call renderQuizResults()
+
 
 }
-
 
 /******************************************************** 
  * Step 3: Define event listeners
  ********************************************************/
-//Listen for clicking the user button
-//Listen for clicking on the multiple choice radio button
-//
 
 function handleUserButton() {
     //wait on user to click -->
@@ -78,6 +79,21 @@ function renderQuestions() {
       //renderQuizPage()
       //call handleRadioButtonClicked()
       //call handleUserButton();
+}
+
+function renderFeedback() {
+    //get current userChoice
+    //compare to correctAnswer
+    //update currentscore if necessary
+    //STORE['correctAnswer'] = userChoice === QUESTIONS[0].userChoice;
+    //renderQuizPage
+    //call handleUserButton()
+}
+
+function renderQuizResults() {
+    //generateHTML() --> loop through questions using map and compare last two fields
+    //renderQuizPage
+    
 }
 
 function renderQuizPage() {

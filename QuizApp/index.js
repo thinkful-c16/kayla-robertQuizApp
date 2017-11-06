c78b529a4bffb03bd3f72ed07ebcee854e46bde4
Kayla Webb <kaylarwebb@gmail.com>
2017-11-06 14:53:39 -0500

    pseudocode written

--- QuizApp/index.js
+++ QuizApp/index.js
@@ -3,6 +3,10 @@
 
 //define data stores 
 //question, answer1, answer2, answer3, answer4, answer5, correct, userChoice 
+
+/******************************************************** 
+ * Step 1: Define objects & database ********************
+ * ************************************/
 const QUESTIONS = [
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
@@ -21,14 +25,40 @@ const STORE = {
   lastQuestionAnswered: 0,
 };
 
+/******************************************************** 
+ * Step 2: Define functions that process user interaction 
+ ********************************************************/
 
 //template generators
-//...code
 
-//rendering functions
+function renderQuestions() {}
 
-//..code
 
-//Event handlers
+/******************************************************** 
+ * Step 3: Define functions that do have user interaction 
+ ********************************************************/
+//Listen for clicking the user button
+//Listen for clicking on the multiple choice radio button
+//
 
-//...code
+function handleUserButton() {};
+function handleRadioButtonClicked() {};
+
+/******************************************************** 
+* Utility housekeeping functions 
+********************************************************/
+
+function renderQuizPage()
+
+/******************************************************** 
+ * Step 4: Main function which calls all the others 
+ ********************************************************/ 
+
+//  function main() {
+    handleUserButton();
+}
+
+// Start the main loop when the web page finishes loading.
+$(function(){
+  handleAnswerSubmitted();
+});

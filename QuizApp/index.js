7b93cb549d67ec9dc614c9a51bed0e886b1600f6
Kayla Webb <kaylarwebb@gmail.com>
2017-11-06 14:20:40 -0500

    organized data stores

--- QuizApp/index.js
+++ QuizApp/index.js
@@ -8,9 +8,18 @@ const QUESTIONS = [
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
   {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0}];
 
-const STORE = {};//user choice; correct answer, current view, score//
+const STORE = {
+  currentView: 0,
+  currentScore: 0,
+  lastQuestionAnswered: 0,
+};
 
 
 //template generators

823d8e7bddc97341eaa853aac131633babc56139
Kayla Webb <kaylarwebb@gmail.com>
2017-11-06 14:14:31 -0500

    code for the questions data set

---
+++ QuizApp/index.js
@@ -0,0 +1,25 @@
+/* global $ */
+'use strict';
+
+//define data stores 
+//question, answer1, answer2, answer3, answer4, answer5, correct, userChoice 
+const QUESTIONS = [
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0},
+  {question: '', answer1: '', answer2: '', answer3: '', answer4: '', answer5: '', correct: 0 , userChoice: 0}];
+
+const STORE = {};//user choice; correct answer, current view, score//
+
+
+//template generators
+//...code
+
+//rendering functions
+
+//..code
+
+//Event handlers
+
+//...code
